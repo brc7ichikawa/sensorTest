3b46e7a677325f9c811b91fe1011ab0cd469fe32
testichikawa <tichikawa@brc7sft.sakura.ne.jp>
2015-06-26 20:05:50 +0900

    Initial Commit

---
+++ sensorTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sensorTest
+//
+//  Created by プレイズム on 2015/06/26.
+//  Copyright (c) 2015年 プレイズム. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
