b13a4d9ac2b6e9c112dad6ca420fde4202597a81
testichikawa <tichikawa@brc7sft.sakura.ne.jp>
2017-06-28 14:06:26 +0900

    ヘッダ変更

--- sensorTest/Define.swift
+++ sensorTest/Define.swift
@@ -2,8 +2,8 @@
 //  Define.swift
 //  sensorTest
 //
-//  Created by プレイズム on 2015/08/19.
-//  Copyright (c) 2015年 プレイズム. All rights reserved.
+//  Created by brc7tichikawa on 2015/08/19.
+//  Copyright (c) 2015年 brc7tichikawa. All rights reserved.
 //
 
 import Foundation
